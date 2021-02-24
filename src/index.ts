838100d9374a4ac462078430388b81940dedbe9f
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2021-02-23 21:47:03 -0500

    add introspection

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ async function main() {
   const schema = await buildSchema({
     resolvers: [TodoItemResolver,TodoResolver]
   })
-  const server = new ApolloServer({ cors: true, schema, playground: {    endpoint: "/dev/graphql"  }})
+  const server = new ApolloServer({ cors: true, schema, introspection: true, playground: {    endpoint: "/dev/graphql"  }})
   await server.listen(process.env.PORT || 4000)
   console.log("Server has started!")
 }
